873cf651189b946360861730bbfe249bbbde1e32
saramohamed124 <saramohamaden1234@gmail.com>
2024-02-04 22:38:30 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -237,3 +237,13 @@ console.log(e.target.parentNode)
 document.querySelector(".popup-overlay").remove()
   }
 })
+// Select All Bullets
+const allBullets = document.querySelectorAll(".nav-bullets .bullet");
+
+allBullets.forEach(bullet=>{
+  bullet.addEventListener('click', (e)=>{
+    document.querySelector(e.target.dataset.section).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+})
